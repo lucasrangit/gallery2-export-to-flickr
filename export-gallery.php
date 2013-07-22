85be5662368ec30833e18b205117722bdfe560f3
Lucas Magasweran <external+github.com@magasweran.com>
2013-07-21 18:59:01 -0700

    fix indentation

--- export-gallery.php
+++ export-gallery.php
@@ -46,15 +46,15 @@
 
 // Read configuration file
 if ( 1 >= $argc ) {
-	echo "Please pass location of a config.php\n";
-	exit(1);
-}
-else {
-	require_once("$argv[1]");
-	if ( empty($DATABASE_HOST) ) {
 		echo "Please pass location of a config.php\n";
 		exit(1);
-	}
+}
+else {
+		require_once("$argv[1]");
+		if ( empty($DATABASE_HOST) ) {
+				echo "Please pass location of a config.php\n";
+				exit(1);
+		}
 }
 
 // ------------- You shouldn't need to modify anything below this line: -------------
@@ -74,101 +74,101 @@ $fes->setToken(TOKEN);
 
 $link = mysql_connect(DATABASE_HOST,DATABASE_USER,DATABASE_PASS);
 if (!$link) {
-	die("Error: Couldn't connect to database server. MySQL said: ".mysql_error());
+		die("Error: Couldn't connect to database server. MySQL said: ".mysql_error());
 }
 
 $db = mysql_select_db(DATABASE_DB,$link);
 
 if (!$db) {
-	die("Error: Couldn't select the database. MySQL said: ".mysql_error());
+		die("Error: Couldn't select the database. MySQL said: ".mysql_error());
 }
 
 
 // skip NULL pathComponents because it probably means that we've either found a really messed
 // up album that I'm not going to deal with, or that we've found the "Gallery" album
 $query = "SELECT i.".DATABASE_COLUMN_PREFIX."title, 
-i.".DATABASE_COLUMN_PREFIX."id, i.".DATABASE_COLUMN_PREFIX."description, 
-fse.".DATABASE_COLUMN_PREFIX."pathComponent FROM ".DATABASE_TABLE_PREFIX."Item i INNER JOIN 
-".DATABASE_TABLE_PREFIX."FileSystemEntity fse ON i.".DATABASE_COLUMN_PREFIX."id = 
-fse.".DATABASE_COLUMN_PREFIX."id WHERE i.".DATABASE_COLUMN_PREFIX."canContainChildren=1 AND 
-i.".DATABASE_COLUMN_PREFIX."ownerId=10 AND 
-fse.".DATABASE_COLUMN_PREFIX."pathComponent IS NOT NULL ORDER BY i.".DATABASE_COLUMN_PREFIX."id";
-//echo $query;
-$result = mysql_query($query);
-echo "<ul>";
+		i.".DATABASE_COLUMN_PREFIX."id, i.".DATABASE_COLUMN_PREFIX."description, 
+		fse.".DATABASE_COLUMN_PREFIX."pathComponent FROM ".DATABASE_TABLE_PREFIX."Item i INNER JOIN 
+		".DATABASE_TABLE_PREFIX."FileSystemEntity fse ON i.".DATABASE_COLUMN_PREFIX."id = 
+		fse.".DATABASE_COLUMN_PREFIX."id WHERE i.".DATABASE_COLUMN_PREFIX."canContainChildren=1 AND 
+		i.".DATABASE_COLUMN_PREFIX."ownerId=10 AND 
+		fse.".DATABASE_COLUMN_PREFIX."pathComponent IS NOT NULL ORDER BY i.".DATABASE_COLUMN_PREFIX."id";
+		//echo $query;
+		$result = mysql_query($query);
+		echo "<ul>";
 
 
-while ($row = mysql_fetch_assoc($result)) {
-  $query = "SELECT * FROM ".DATABASE_TABLE_PREFIX."ChildEntity i INNER JOIN " .DATABASE_TABLE_PREFIX."PhotoItem fse ON i." .DATABASE_COLUMN_PREFIX."id = fse.".DATABASE_COLUMN_PREFIX."id where i.g_parentId = ".$row[DATABASE_COLUMN_PREFIX."id"];
-  //echo "\n$query\n";
-  $result_temp = mysql_query($query) or print(mysql_error());
-  if (mysql_num_rows($result_temp)==0) continue;
-  //  if ($row[DATABASE_COLUMN_PREFIX."id"] < 94950) continue; //used to continue the script from a specific album after it prematurely ends
+		while ($row = mysql_fetch_assoc($result)) {
+				$query = "SELECT * FROM ".DATABASE_TABLE_PREFIX."ChildEntity i INNER JOIN " .DATABASE_TABLE_PREFIX."PhotoItem fse ON i." .DATABASE_COLUMN_PREFIX."id = fse.".DATABASE_COLUMN_PREFIX."id where i.g_parentId = ".$row[DATABASE_COLUMN_PREFIX."id"];
+				//echo "\n$query\n";
+				$result_temp = mysql_query($query) or print(mysql_error());
+				if (mysql_num_rows($result_temp)==0) continue;
+				//  if ($row[DATABASE_COLUMN_PREFIX."id"] < 94950) continue; //used to continue the script from a specific album after it prematurely ends
 
-	echo "<li>Album Title: ".$row[DATABASE_COLUMN_PREFIX."title"]."</li>";
-	echo "<li>Album Id: ".$row[DATABASE_COLUMN_PREFIX."id"]."</li>";
-	echo "<li>Album Path: ".$row[DATABASE_COLUMN_PREFIX."pathComponent"]."</li>";
-	echo "<li>Description: ".$row[DATABASE_COLUMN_PREFIX."description"]."<br/>";
-	$uploadedPics=array();
+				echo "<li>Album Title: ".$row[DATABASE_COLUMN_PREFIX."title"]."</li>";
+				echo "<li>Album Id: ".$row[DATABASE_COLUMN_PREFIX."id"]."</li>";
+				echo "<li>Album Path: ".$row[DATABASE_COLUMN_PREFIX."pathComponent"]."</li>";
+				echo "<li>Description: ".$row[DATABASE_COLUMN_PREFIX."description"]."<br/>";
+				$uploadedPics=array();
 
-$query = "SELECT i.".DATABASE_COLUMN_PREFIX."id, i.".DATABASE_COLUMN_PREFIX."title, i.".DATABASE_COLUMN_PREFIX."description, fse.".DATABASE_COLUMN_PREFIX."pathComponent 
-FROM ".DATABASE_TABLE_PREFIX."Item i 
-INNER JOIN ".DATABASE_TABLE_PREFIX."ChildEntity ce ON i.".DATABASE_COLUMN_PREFIX."id = ce.".DATABASE_COLUMN_PREFIX."id 
-INNER JOIN ".DATABASE_TABLE_PREFIX."FileSystemEntity fse ON i.".DATABASE_COLUMN_PREFIX."id = fse.".DATABASE_COLUMN_PREFIX."id 
-INNER JOIN ".DATABASE_TABLE_PREFIX."PhotoItem gse ON i.".DATABASE_COLUMN_PREFIX."id = gse.".DATABASE_COLUMN_PREFIX."id 
-WHERE ce.".DATABASE_COLUMN_PREFIX."parentId=".$row[DATABASE_COLUMN_PREFIX."id"].
-" ORDER BY i.".DATABASE_COLUMN_PREFIX."id";
- $childern = mysql_query($query);
-	echo "<ul>";
-	while ($child = mysql_fetch_assoc($childern)) {
-	  $query = "SELECT i.".DATABASE_COLUMN_PREFIX."parentSequence FROM ".DATABASE_TABLE_PREFIX."ItemAttributesMap i WHERE i.".DATABASE_COLUMN_PREFIX."itemId =".$child[DATABASE_COLUMN_PREFIX."id"];
-	  $result_temp = mysql_query($query);
-	  $temp = mysql_fetch_array($result_temp);
-	  $path = BASE_DIRECTORY."/albums/".fullpath($temp[0],$child[DATABASE_COLUMN_PREFIX."id"]);
-		echo "<li>".$child[DATABASE_COLUMN_PREFIX."title"]." -- ".$child[DATABASE_COLUMN_PREFIX."pathComponent"]."<br/>".$child[DATABASE_COLUMN_PREFIX."description"]." File is ".$path."</li>";
-		//	continue;
+				$query = "SELECT i.".DATABASE_COLUMN_PREFIX."id, i.".DATABASE_COLUMN_PREFIX."title, i.".DATABASE_COLUMN_PREFIX."description, fse.".DATABASE_COLUMN_PREFIX."pathComponent 
+						FROM ".DATABASE_TABLE_PREFIX."Item i 
+						INNER JOIN ".DATABASE_TABLE_PREFIX."ChildEntity ce ON i.".DATABASE_COLUMN_PREFIX."id = ce.".DATABASE_COLUMN_PREFIX."id 
+						INNER JOIN ".DATABASE_TABLE_PREFIX."FileSystemEntity fse ON i.".DATABASE_COLUMN_PREFIX."id = fse.".DATABASE_COLUMN_PREFIX."id 
+						INNER JOIN ".DATABASE_TABLE_PREFIX."PhotoItem gse ON i.".DATABASE_COLUMN_PREFIX."id = gse.".DATABASE_COLUMN_PREFIX."id 
+						WHERE ce.".DATABASE_COLUMN_PREFIX."parentId=".$row[DATABASE_COLUMN_PREFIX."id"].
+						" ORDER BY i.".DATABASE_COLUMN_PREFIX."id";
+				$childern = mysql_query($query);
+				echo "<ul>";
+				while ($child = mysql_fetch_assoc($childern)) {
+						$query = "SELECT i.".DATABASE_COLUMN_PREFIX."parentSequence FROM ".DATABASE_TABLE_PREFIX."ItemAttributesMap i WHERE i.".DATABASE_COLUMN_PREFIX."itemId =".$child[DATABASE_COLUMN_PREFIX."id"];
+						$result_temp = mysql_query($query);
+						$temp = mysql_fetch_array($result_temp);
+						$path = BASE_DIRECTORY."/albums/".fullpath($temp[0],$child[DATABASE_COLUMN_PREFIX."id"]);
+						echo "<li>".$child[DATABASE_COLUMN_PREFIX."title"]." -- ".$child[DATABASE_COLUMN_PREFIX."pathComponent"]."<br/>".$child[DATABASE_COLUMN_PREFIX."description"]." File is ".$path."</li>";
+						//	continue;
 
-				$uploadedPics[]=$f->sync_upload(
-								html_entity_decode($path),
-				html_entity_decode($child[DATABASE_COLUMN_PREFIX."title"]),
-				html_entity_decode($child[DATABASE_COLUMN_PREFIX."description"]),
-				null, // tags
-				true); // public 
+						$uploadedPics[]=$f->sync_upload(
+										html_entity_decode($path),
+										html_entity_decode($child[DATABASE_COLUMN_PREFIX."title"]),
+										html_entity_decode($child[DATABASE_COLUMN_PREFIX."description"]),
+										null, // tags
+										true); // public 
 
-		if (count($uploadedPics)%8) {
-			// every 8 photos, give flickr a break and a chance
-			// to catch up. Adjust this value if you need to.
-			// Without this sleep, the connection dropped a lot
-			// and I had to keep reuploading the same photos
-		  			sleep(3);
+						if (count($uploadedPics)%8) {
+								// every 8 photos, give flickr a break and a chance
+								// to catch up. Adjust this value if you need to.
+								// Without this sleep, the connection dropped a lot
+								// and I had to keep reuploading the same photos
+								sleep(3);
+						}
+				}
+				//	sleep(2);
+				$setid=$fes->photosets_create(html_entity_decode($row[DATABASE_COLUMN_PREFIX."title"]),html_entity_decode($row[DATABASE_COLUMN_PREFIX."description"]),$uploadedPics[0]);
+				foreach($uploadedPics as $pid) {
+						echo "adding $pid<br/>";
+						$fes->photosets_addPhoto($setid['id'],$pid);
+				}
+				echo "</li></ul>";
+				//	sleep(3); // take a good fitful sleep after uploading a whole album
 		}
-	}
-	//	sleep(2);
-	$setid=$fes->photosets_create(html_entity_decode($row[DATABASE_COLUMN_PREFIX."title"]),html_entity_decode($row[DATABASE_COLUMN_PREFIX."description"]),$uploadedPics[0]);
-	foreach($uploadedPics as $pid) {
-		echo "adding $pid<br/>";
-		$fes->photosets_addPhoto($setid['id'],$pid);
-	}
-	echo "</li></ul>";
-	//	sleep(3); // take a good fitful sleep after uploading a whole album
- }
 echo "</ul>";
 
 function fullpath($parents,$id){
-  $pieces = explode("/", $parents);
-  $full_path = "";
-  foreach($pieces as $parent){
-    if($parent == "") continue;
-    $query = "SELECT i.".DATABASE_COLUMN_PREFIX."pathComponent FROM ".DATABASE_TABLE_PREFIX."FileSystemEntity i WHERE i.".DATABASE_COLUMN_PREFIX."id =".$parent;
-    $result_temp = mysql_query($query);
-    $temp = mysql_fetch_array($result_temp);
-    $full_path = $full_path . "/" . $temp[0];
-  }
-    $query = "SELECT i.".DATABASE_COLUMN_PREFIX."pathComponent FROM ".DATABASE_TABLE_PREFIX."FileSystemEntity i WHERE i.".DATABASE_COLUMN_PREFIX."id =".$id;
-    $result_temp = mysql_query($query);
-    $temp = mysql_fetch_array($result_temp);
-    $full_path = $full_path . "/" . $temp[0];
-    return $full_path;
+		$pieces = explode("/", $parents);
+		$full_path = "";
+		foreach($pieces as $parent){
+				if($parent == "") continue;
+				$query = "SELECT i.".DATABASE_COLUMN_PREFIX."pathComponent FROM ".DATABASE_TABLE_PREFIX."FileSystemEntity i WHERE i.".DATABASE_COLUMN_PREFIX."id =".$parent;
+				$result_temp = mysql_query($query);
+				$temp = mysql_fetch_array($result_temp);
+				$full_path = $full_path . "/" . $temp[0];
+		}
+		$query = "SELECT i.".DATABASE_COLUMN_PREFIX."pathComponent FROM ".DATABASE_TABLE_PREFIX."FileSystemEntity i WHERE i.".DATABASE_COLUMN_PREFIX."id =".$id;
+		$result_temp = mysql_query($query);
+		$temp = mysql_fetch_array($result_temp);
+		$full_path = $full_path . "/" . $temp[0];
+		return $full_path;
 }
 
 mysql_close($link);
